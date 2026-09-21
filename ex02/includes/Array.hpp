125b0a2ddcc595c39511ad56627d160074ea4a79
pmalumba <pmalumba@f2r6s2.paris.42.school>
2026-09-21 18:09:20 +0200

    42

--- ex02/includes/Array.hpp
+++ ex02/includes/Array.hpp
@@ -5,9 +5,38 @@
 
 template <typename T>
 class Array{
+private:
+    T* _data;
+    unsigned int _size;
 public:
+    Array(){
+        _data = new T[];
+    }
     
+    Array(unsigned int n) : _size(n){
+        _data = new T[n];
+    }
 
+    Array(const Array& src) : _data(NULL){
+        *this = src;
+    }
+
+    Array& operator=(const Array& src){
+        if (this != &src){
+            delete[] _data;
+            this->_size = src._size;
+            this->_data = new T[_size];
+            for (unsigned int v = 0; v < _size; v++)
+                this->_data[v] = src._data[v];
+        }
+        return *this;
+    }
+
+    ~Array() {};
+
+    size() const{
+        return (this->_size);
+    }
 };
 
 #endif
